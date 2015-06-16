4e50eb4792c9020577f5b6e1eeeacfefcd6e5bdc
Ed Brannin <ed.brannin@thomsonreuters.com>
2015-06-16 14:25:17 -0400

    Rendering the template

--- matt_ebay.py
+++ matt_ebay.py
@@ -6,10 +6,12 @@ from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader("."))
 
 def main(csv_filename, template_filename):
+    template = env.get_template(template_filename)
     with open(csv_filename, 'rU') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            print row
+            print template.render(**row)
+            break
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
